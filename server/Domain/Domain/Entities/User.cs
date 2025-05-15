eefa71b369aa30580159738502e34c80aebd6895
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-05-15 12:25:19 +0200

    update database schema for tasks

--- server/Domain/Domain/Entities/User.cs
+++ server/Domain/Domain/Entities/User.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public string UserName { get; set; }
         public string Email { get; set; }
+        ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
     }
 }   
--- server/Infrastructure/Data/Entities/TaskEntity.cs
+++ server/Infrastructure/Data/Entities/TaskEntity.cs
@@ -5,8 +5,11 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string UserId { get; set; }
         public bool IsCompleted { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? CompletedAt { get; set; }
+
+        public ApplicationUser User { get; set; } = null!;
     }
 }
