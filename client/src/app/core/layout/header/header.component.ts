dd3866c17f07acf9b98eec65110b184a8181d1d7
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-06-27 12:29:54 +0200

    start creating login form

--- client/src/app/core/layout/header/header.component.ts
+++ client/src/app/core/layout/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
--- client/src/app/features/auth/pages/login-page/login-page.component.ts
+++ client/src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { M } from '@angular/cdk/keycodes';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
 
+  errorMessage = signal("");
+  updateErrorMessage() {
+    if (this.loginForm.get("email")?.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.loginForm.get("email")?.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  onSubmit() {
+    console.log('Form Submitted', this.loginForm.value);
+  }
 }
